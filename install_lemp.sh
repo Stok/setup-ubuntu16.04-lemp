18dd3ac8fa52ba675dfae1beaab36e1b5b9fefde
Stok <sean@stokedprojects.org>
2018-05-11 20:24:10 +0200

    ip injection was missing parentheses

--- install_lemp.sh
+++ install_lemp.sh
@@ -13,11 +13,11 @@ sudo apt-get -y install mysql-server
 
 #PHP
 sudo apt-get -y install php-fpm php-mysql
-sudo cat /etc/php/7.0/fpm/php.ini | sed "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" | sudo tee /etc/php/7.0/fpm/php.ini > /dev/null
+cat /etc/php/7.0/fpm/php.ini | sed "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" | sudo tee /etc/php/7.0/fpm/php.ini > /dev/null
 sudo systemctl restart php7.0-fpm
 
 # Configure nginx so it uses PHP
-ip=ifconfig eth0 | grep "inet adr" | cut -d ':' -f 2 | cut -d ' ' -f 1
+ip=$(ifconfig eth0 | grep "inet adr" | cut -d ':' -f 2 | cut -d ' ' -f 1)
 cat ./nginx_sites_available_default | sed "s/server_name _;/server_name $ip;/g" > ./nginx_sites_available_default
 sudo chown root ./nginx_sites_available_default
 sudo cp ./nginx_sites_available_default /etc/nginx/sites-available/default
