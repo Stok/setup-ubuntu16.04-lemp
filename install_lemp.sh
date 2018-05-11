f099b308c3373f1ca405f6f6b8d8abd78435ff48
Stok <sean@stokedprojects.org>
2018-05-11 21:30:58 +0200

    add tee to ip

--- install_lemp.sh
+++ install_lemp.sh
@@ -18,7 +18,7 @@ sudo systemctl restart php7.0-fpm
 
 # Configure nginx so it uses PHP
 ip=$(ifconfig eth0 | grep "inet adr" | cut -d ':' -f 2 | cut -d ' ' -f 1)
-cat ./nginx_sites_available_default | sed "s/server_name _;/server_name $ip;/g" > ./nginx_sites_available_default
+cat ./nginx_sites_available_default | sed "s/server_name _;/server_name $ip;/g" | sudo tee ./nginx_sites_available_default > /dev/null
 sudo chown root ./nginx_sites_available_default
 sudo chgrp root ./nginx_sites_available_default
 sudo cp ./nginx_sites_available_default /etc/nginx/sites-available/default
