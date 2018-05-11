fa10b2bd4b10cd82ce961ebee8d9b6ec96311520
Stok <sean@stokedprojects.org>
2018-05-11 21:41:17 +0200

    simplify chown

--- install_lemp.sh
+++ install_lemp.sh
@@ -14,8 +14,7 @@ sudo apt-get -y install mysql-server
 #PHP
 sudo apt-get -y install php-fpm php-mysql
 cat /etc/php/7.0/fpm/php.ini | sed "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" > ./php.ini
-sudo chown root ./php.ini
-sudo chgrp root ./php.ini
+sudo chown root:root ./php.ini
 sudo cp ./php.ini /etc/php/7.0/fpm/php.ini
 sudo rm ./php.ini
 sudo systemctl restart php7.0-fpm
@@ -23,8 +22,7 @@ sudo systemctl restart php7.0-fpm
 # Configure nginx so it uses PHP
 ip=$(ifconfig eth0 | grep "inet adr" | cut -d ':' -f 2 | cut -d ' ' -f 1)
 cat ./nginx_sites_available_default | sed "s/server_name _;/server_name $ip;/g" > ./default
-sudo chown root ./default
-sudo chgrp root ./default
+sudo chown root:root ./default
 sudo cp ./default /etc/nginx/sites-available/default
 sudo rm ./default
 sudo nginx -t
