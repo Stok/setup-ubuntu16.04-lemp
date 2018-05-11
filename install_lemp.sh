d333a680f28c5fba59eacbf011ebb2880c6cffeb
Stok <sean@stokedprojects.org>
2018-05-11 20:06:28 +0200

    added -y to apt-get installs and add sudo tee to allow copy line 16

--- install_lemp.sh
+++ install_lemp.sh
@@ -5,15 +5,15 @@ sudo apt-get update
 sudo apt-get -y dist-upgrade
 
 # Nginx
-sudo apt-get install nginx
+sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 
 # MySQL
-sudo apt-get install mysql-server
+sudo apt-get -y install mysql-server
 
 #PHP
-sudo apt-get install php-fpm php-mysql
-sudo cat /etc/php/7.0/fpm/php.ini | sed "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" > /etc/php/7.0/fpm/php.ini
+sudo apt-get -y install php-fpm php-mysql
+sudo cat /etc/php/7.0/fpm/php.ini | sed "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" > sudo tee /etc/php/7.0/fpm/php.ini > /dev/null
 sudo systemctl restart php7.0-fpm
 
 # Configure nginx so it uses PHP
